4d25cac0586d08fd3c63289a0f2a147e8ee4988b
Bobbie Goede <bobbiegoede@gmail.com>
2024-01-06 10:40:12 +0100

    chore: update deps

--- app.vue
+++ app.vue
@@ -3,10 +3,9 @@
 </template>
 
 <script lang="ts" setup>
-const { $i18n } = useNuxtApp()
+const { $i18n } = useNuxtApp();
 
-await new Promise(resolve => setTimeout(resolve, 10))
+await new Promise((resolve) => setTimeout(resolve, 10));
 
-const timeLabel = computed(() => $i18n.t('general'))
+const timeLabel = computed(() => $i18n.t("general"));
 </script>
-
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/i18n'],
+  modules: ["@nuxtjs/i18n"],
 
   i18n: {
-    defaultLocale: 'eng',
-    langDir: './locales',
+    defaultLocale: "eng",
+    langDir: "./locales",
+    // debug: true,
     locales: [
       {
-        code: 'eng',
-        file: 'en.json',
-      }
+        code: "eng",
+        file: "en.json",
+      },
     ],
   },
-})
+});
